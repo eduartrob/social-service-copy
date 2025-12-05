ddce55b9a07b22938a78a8adbcfecfc4947fbf5d
eduartrob <eduartrob@gmail.com>
2025-12-04 23:33:14 -0600

    fix: corregir relaciones de modelos para usar targetKey: user_id

--- src/infrastructure/database/models/index.js
+++ src/infrastructure/database/models/index.js
@@ -24,68 +24,70 @@ const CommunityMemberModel = require('./CommunityMemberModel')(sequelize, Sequel
 // Definir asociaciones entre modelos
 
 // UserProfile - Interest (1:N)
-UserProfileModel.hasMany(InterestModel, { 
-  foreignKey: 'user_id', 
+UserProfileModel.hasMany(InterestModel, {
+  foreignKey: 'user_id',
   as: 'interests',
   onDelete: 'CASCADE'
 });
-InterestModel.belongsTo(UserProfileModel, { 
-  foreignKey: 'user_id', 
+InterestModel.belongsTo(UserProfileModel, {
+  foreignKey: 'user_id',
   as: 'userProfile'
 });
 
 // UserProfile - Publication (1:N)
-UserProfileModel.hasMany(PublicationModel, { 
-  foreignKey: 'user_id', 
+UserProfileModel.hasMany(PublicationModel, {
+  foreignKey: 'user_id',
   as: 'publications',
   onDelete: 'CASCADE'
 });
-PublicationModel.belongsTo(UserProfileModel, { 
-  foreignKey: 'user_id', 
+PublicationModel.belongsTo(UserProfileModel, {
+  foreignKey: 'user_id',
+  targetKey: 'user_id',
   as: 'author'
 });
 
 // Publication - Comment (1:N)
-PublicationModel.hasMany(CommentModel, { 
-  foreignKey: 'post_id', 
+PublicationModel.hasMany(CommentModel, {
+  foreignKey: 'post_id',
   as: 'comments',
   onDelete: 'CASCADE'
 });
-CommentModel.belongsTo(PublicationModel, { 
-  foreignKey: 'post_id', 
+CommentModel.belongsTo(PublicationModel, {
+  foreignKey: 'post_id',
   as: 'publication'
 });
 
 // Comment - Comment (1:N) - Para respuestas a comentarios
-CommentModel.hasMany(CommentModel, { 
-  foreignKey: 'parent_id', 
+CommentModel.hasMany(CommentModel, {
+  foreignKey: 'parent_id',
   as: 'replies',
   onDelete: 'CASCADE'
 });
-CommentModel.belongsTo(CommentModel, { 
-  foreignKey: 'parent_id', 
+CommentModel.belongsTo(CommentModel, {
+  foreignKey: 'parent_id',
   as: 'parentComment'
 });
 
 // UserProfile - Comment (1:N)
-UserProfileModel.hasMany(CommentModel, { 
-  foreignKey: 'user_id', 
+UserProfileModel.hasMany(CommentModel, {
+  foreignKey: 'user_id',
   as: 'comments',
   onDelete: 'CASCADE'
 });
-CommentModel.belongsTo(UserProfileModel, { 
-  foreignKey: 'user_id', 
+CommentModel.belongsTo(UserProfileModel, {
+  foreignKey: 'user_id',
+  targetKey: 'user_id',
   as: 'author'
 });
 
 // Publication - MediaItem (1:N)
-PublicationModel.hasMany(MediaItemModel, { 
-  foreignKey: 'post_id', 
+PublicationModel.hasMany(MediaItemModel, {
+  foreignKey: 'post_id',
   as: 'mediaItems',
   onDelete: 'CASCADE'
 });
-MediaItemModel.belongsTo(PublicationModel, { 
-  foreignKey: 'post_id', 
+MediaItemModel.belongsTo(PublicationModel, {
+  foreignKey: 'post_id',
   as: 'publication'
 });
 
@@ -101,7 +103,7 @@ PublicationModel.hasMany(LikeModel, {
 
 // Comment - Like (1:N) usando el sistema polymórfico  
 CommentModel.hasMany(LikeModel, {
-  foreignKey: 'likeable_id', 
+  foreignKey: 'likeable_id',
   constraints: false,
   scope: {
     likeable_type: 'comment'
@@ -110,70 +112,70 @@ CommentModel.hasMany(LikeModel, {
 });
 
 // UserProfile - Like (1:N)
-UserProfileModel.hasMany(LikeModel, { 
-  foreignKey: 'user_id', 
+UserProfileModel.hasMany(LikeModel, {
+  foreignKey: 'user_id',
   as: 'likes',
   onDelete: 'CASCADE'
 });
-LikeModel.belongsTo(UserProfileModel, { 
-  foreignKey: 'user_id', 
+LikeModel.belongsTo(UserProfileModel, {
+  foreignKey: 'user_id',
   as: 'user'
 });
 
 // Definir asociaciones para los nuevos modelos
 
 // UserProfile - UserPreference (1:1)
-UserProfileModel.hasOne(UserPreferenceModel, { 
-  foreignKey: 'user_id', 
+UserProfileModel.hasOne(UserPreferenceModel, {
+  foreignKey: 'user_id',
   as: 'userPreferences',
   onDelete: 'CASCADE'
 });
-UserPreferenceModel.belongsTo(UserProfileModel, { 
-  foreignKey: 'user_id', 
+UserPreferenceModel.belongsTo(UserProfileModel, {
+  foreignKey: 'user_id',
   as: 'user'
 });
 
 // UserProfile - CompleteProfile (1:1)
-UserProfileModel.hasOne(CompleteProfileModel, { 
-  foreignKey: 'user_id', 
+UserProfileModel.hasOne(CompleteProfileModel, {
+  foreignKey: 'user_id',
   as: 'completeProfile',
   onDelete: 'CASCADE'
 });
-CompleteProfileModel.belongsTo(UserProfileModel, { 
-  foreignKey: 'user_id', 
+CompleteProfileModel.belongsTo(UserProfileModel, {
+  foreignKey: 'user_id',
   as: 'user'
 });
 
 // UserProfile - Community (1:N) - Como creador
-UserProfileModel.hasMany(CommunityModel, { 
-  foreignKey: 'creator_id', 
+UserProfileModel.hasMany(CommunityModel, {
+  foreignKey: 'creator_id',
   as: 'createdCommunities',
   onDelete: 'CASCADE'
 });
-CommunityModel.belongsTo(UserProfileModel, { 
-  foreignKey: 'creator_id', 
+CommunityModel.belongsTo(UserProfileModel, {
+  foreignKey: 'creator_id',
   as: 'creator'
 });
 
 // Community - CommunityMember (1:N)
-CommunityModel.hasMany(CommunityMemberModel, { 
-  foreignKey: 'community_id', 
+CommunityModel.hasMany(CommunityMemberModel, {
+  foreignKey: 'community_id',
   as: 'memberships',
   onDelete: 'CASCADE'
 });
-CommunityMemberModel.belongsTo(CommunityModel, { 
-  foreignKey: 'community_id', 
+CommunityMemberModel.belongsTo(CommunityModel, {
+  foreignKey: 'community_id',
   as: 'community'
 });
 
 // UserProfile - CommunityMember (1:N)
-UserProfileModel.hasMany(CommunityMemberModel, { 
-  foreignKey: 'user_id', 
+UserProfileModel.hasMany(CommunityMemberModel, {
+  foreignKey: 'user_id',
   as: 'communityMemberships',
   onDelete: 'CASCADE'
 });
-CommunityMemberModel.belongsTo(UserProfileModel, { 
-  foreignKey: 'user_id', 
+CommunityMemberModel.belongsTo(UserProfileModel, {
+  foreignKey: 'user_id',
   as: 'user'
 });
 
@@ -192,24 +194,24 @@ CommunityModel.belongsToMany(UserProfileModel, {
 });
 
 // UserProfile - Friendship (1:N) - Como solicitante
-UserProfileModel.hasMany(FriendshipModel, { 
-  foreignKey: 'requester_id', 
+UserProfileModel.hasMany(FriendshipModel, {
+  foreignKey: 'requester_id',
   as: 'sentFriendRequests',
   onDelete: 'CASCADE'
 });
-FriendshipModel.belongsTo(UserProfileModel, { 
-  foreignKey: 'requester_id', 
+FriendshipModel.belongsTo(UserProfileModel, {
+  foreignKey: 'requester_id',
   as: 'requester'
 });
 
 // UserProfile - Friendship (1:N) - Como destinatario
-UserProfileModel.hasMany(FriendshipModel, { 
-  foreignKey: 'addressee_id', 
+UserProfileModel.hasMany(FriendshipModel, {
+  foreignKey: 'addressee_id',
   as: 'receivedFriendRequests',
   onDelete: 'CASCADE'
 });
-FriendshipModel.belongsTo(UserProfileModel, { 
-  foreignKey: 'addressee_id', 
+FriendshipModel.belongsTo(UserProfileModel, {
+  foreignKey: 'addressee_id',
   as: 'addressee'
 });
 
